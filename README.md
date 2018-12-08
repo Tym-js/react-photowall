1f8445caa45d96eea495c0ec186e5aa2f8aa7e11
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-08 16:55:55 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-photowall
-Created with CodeSandbox
---
+++ src/components/Photo.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+class Photo extends React.Component {
+  render() {
+    const post = this.props.post
+    return <figure className="figure" />
+  }
+}
+
+export default Photo
